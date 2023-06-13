69093ed060350b79ee93c65f0ae0ac5a01d76265
Pietro Giordano <giopi87@gmail.com>
2023-06-13 18:04:17 +0200

    aggiunta di metodi 3 su 5 a ProgrammEventi

--- src/org/lessons/java/ProgrammEventi.java
+++ src/org/lessons/java/ProgrammEventi.java
@@ -11,4 +11,19 @@ public class ProgrammEventi {
         this.titolo = titolo;
         this.eventi = new ArrayList<>();
     }
+
+    // metodo aggiungi evento
+    public void aggiungiEvento(Evento evento) {
+        eventi.add(evento);
+    }
+
+    // metodo che restituisce numero eventi
+    public int getNumeroEventi() {
+        return eventi.size();
+    }
+
+    // metodo che svuota lista
+    public void svuotaEventi() {
+        eventi.clear();
+    }
 }
