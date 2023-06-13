180ab86b5ba9640fd0b6fdcd4c080a3082ade68b
Pietro Giordano <giopi87@gmail.com>
2023-06-13 17:06:18 +0200

    aggiunta classe concerto con validazioni in classe

--- src/org/lessons/java/Concerto.java
+++ src/org/lessons/java/Concerto.java
@@ -1,16 +1,17 @@
 package org.lessons.java;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 public class Concerto extends Evento {
     private LocalTime ora;
     private BigDecimal prezzo;
 
-    public Concerto(String titolo, String data, int nPostiTotali, String ora, BigDecimal prezzo) throws EventoCreateException, InvalidDateException {
+    public Concerto(String titolo, String data, int nPostiTotali, String ora, BigDecimal prezzo) throws EventoCreateException, InvalidDateException, InvalidTimeException, InvalidPriceException {
         super(titolo, data, nPostiTotali);
-        this.ora = LocalTime.parse(ora);
-        this.prezzo = prezzo;
+        this.ora = oraFormattata(ora);
+        this.prezzo = prezzoFormattato(prezzo);
     }
 
     public LocalTime getOra() {
@@ -29,12 +30,26 @@ public class Concerto extends Evento {
         this.prezzo = prezzo;
     }
 
-    public void oraFormattata(String ora) {
+    public LocalTime oraFormattata(String ora) throws InvalidTimeException {
+        String regex = "\\d{2}:\\d{2}";  // validatore
 
+        if(ora.matches(regex)) {
+            return LocalTime.parse(ora);
+        } else {
+            throw new InvalidTimeException();
+        }
+    }
+
+    public BigDecimal prezzoFormattato(BigDecimal prezzo) throws InvalidPriceException {
+        if (prezzo instanceof BigDecimal) {
+            return prezzo.setScale(2, RoundingMode.HALF_UP);
+        } else {
+            throw new InvalidPriceException();
+        }
     }
 
     @Override
     public String toString() {
-        return "Concerto {" + getData() + ora + " - " + getTitolo() + " - " + prezzo + '}';
+        return "Concerto {" + getData() + ora + " - " + getTitolo() + " - " + prezzo + "€ " + '}';
     }
 }
