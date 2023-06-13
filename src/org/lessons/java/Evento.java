6fe532d1f2247a8bdadea679dc759d55c166418f
Pietro Giordano <giopi87@gmail.com>
2023-06-13 11:51:24 +0200

    aggiunta metodo che verifica data non sia passata

--- src/org/lessons/java/Evento.java
+++ src/org/lessons/java/Evento.java
@@ -1,5 +1,9 @@
 package org.lessons.java;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Evento {
     private String titolo;
     private String data;
@@ -37,7 +41,7 @@ public class Evento {
         return nPostiPrenotati;
     }
 
-    public Integer nPositivo(int n) {
+    public Integer nPositivo(int n) throws NumberFormatException {
         try {
             if (n > 0) {
                 return n;
@@ -48,5 +52,31 @@ public class Evento {
             System.out.println("Si prega di inserire un numero!");
         }
         return null;
+    }
+
+    public String comparatoreData(String input) throws DateTimeParseException {
+        String regex = "\\d{2}/\\d{2}/\\d{4}";  // validator
+        try {
+            if(input.matches(regex)) {
+                // formattatore
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                // parso stringa in data formattata
+                LocalDate data = LocalDate.parse(input, formatter);
+                // recupero data odierna
+                LocalDate oggi = LocalDate.now();
+
+                if (data.isAfter(oggi)) {
+                    return input;
+                } else {
+                    System.out.println("La data inserita non è valida in quanto già passata!");
+                    return null;
+                }
+            } else {
+                throw new DateTimeParseException("Data errata", input, 0);
+            }
+        } catch (Exception e) {
+            System.out.println("Formato data non valido");
+            return null;
+        }
     }
 }
